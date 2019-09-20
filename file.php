0e6a6c909047563edf1f09d75f70dd7f6f81cca4
nmr97007 <55343518+nmr97007@users.noreply.github.com>
2019-09-20 11:22:03 +0530

    Update file.php

--- file.php
+++ file.php
@@ -28,7 +28,10 @@ $readonly_users = array(
 
 // user specific directories
 // array('Username' => 'Directory path', 'Username2' => 'Directory path', ...)
-$directories_users = array();
+$directories_users = array(
+'admin' => 'MyFiles',
+'user' => 'Userfiles'
+);
 
 // Enable highlight.js (https://highlightjs.org/) on view's page
 $use_highlightjs = true;
@@ -43,7 +46,7 @@ $edit_files = true;
 $default_timezone = 'Etc/UTC'; // UTC
 
 // Root path for file manager
-$root_path = $_SERVER['/Downloads'];
+$root_path = $_SERVER['DOCUMENT_ROOT'];
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
